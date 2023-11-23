d2ee619a534cd6636b41cadeb87f527222b502f0
Lalo <duranjeda9@gmail.com>
2023-11-23 11:14:49 -0600

    Cambio server

--- node/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getAllRegister } from '../controllers/Registroscontroller.js'
-const router= express.Router()
-
-router.get('/',getAllRegister)
-
-
-export default router
--- node/routes/routeshgo.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getAllRegisterhgo } from '../controllers/Registroscontrollerhgo.js'
-const router= express.Router()
-
-router.get('/',getAllRegisterhgo)
-
-
-export default router
